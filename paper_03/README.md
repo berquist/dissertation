00c961603059bae10d4a9bb5691a104ba547d93c
Eric Berquist <eric.berquist@gmail.com>
2018-01-06 11:18:26 -0500

    paper_03: initial pandoc output

---
+++ paper_03/README.md
@@ -0,0 +1,8 @@
+These files are from `/home/eric/Chemistry/calc.sgr/paper_02_CD_SC/docs`:
+
+```
+pandoc.sh
+SI.pdf
+Spectroscopic_Map_R1.pdf
+Spectroscopic_Map_revised.docx
+```
--- paper_03/pandoc.sh
+++ paper_03/pandoc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # pandoc -s -f markdown_github -t docx -o reviewer_comments_AbInitio.docx reviewer_comments_EJB_CAD.md
-pandoc -s -f markdown -t docx -o reviewer_comments_AbInitio.docx reviewer_comments_EJB_CAD.md
+# pandoc -s -f markdown -t docx -o reviewer_comments_AbInitio.docx reviewer_comments_EJB_CAD.md
+pandoc -s -f docx -t latex -o Spectroscopic_Map_revised.tex Spectroscopic_Map_revised.docx
